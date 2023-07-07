506b8dc0d00e19bdf9cf53bf5fd527af2fa5bae8
IKalonji <IKalonji@student.wethinkcode.co.za>
2023-07-07 18:29:44 +0200

    moving to contract deployement

--- riccardian-ai/src/pages/CreateNew.pages.js
+++ riccardian-ai/src/pages/CreateNew.pages.js
@@ -12,8 +12,8 @@ const CreateNew = () => {
 
     const footerContent = (
         <div>
-            <Button label="Paste" icon="pi pi-times" onClick={() => { setSelectedContent("paste"); setVisible(false); }} autoFocus />
-            <Button label="Manually enter content" icon="pi pi-check" onClick={() => { setSelectedContent("manually"); setVisible(false); }} autoFocus />
+            <Button label="Paste" icon="pi pi-file-import" onClick={() => { setSelectedContent("paste"); setVisible(false); }} autoFocus />
+            <Button label="Manually enter content" icon="pi pi-file-edit" onClick={() => { setSelectedContent("manually"); setVisible(false); }} autoFocus />
         </div>
     );
 
@@ -33,45 +33,72 @@ const CreateNew = () => {
         const header = renderHeader();
       
         return (
-          <div>
-            <Editor value={editorText} onTextChange={(e) => setEditorText(e.textValue)} headerTemplate={header} style={{ height: '320px' }} />
-          </div>
+          
+        <div className="flex align-items-center justify-content-center">
+            <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+                <div className="text-center mb-5">
+                    <img src="/demo/images/blocks/logos/hyper.svg" alt="hyper" height={50} className="mb-3" />
+                    <div className="text-900 text-3xl font-medium mb-3">Paste the Human readable Contract here</div>
+                </div>
+
+                <div>
+                    <div className="spacer" style={{height:"20px"}}></div>
+                    <Editor value={editorText} onTextChange={(e) => { setEditorText(e.textValue) }} headerTemplate={header} style={{ height: '320px' }} />
+                    <div className="spacer" style={{height:"20px"}}></div>
+                    <Button label="Save and review" icon="pi pi-cloud-upload" className="w-full" />
+                 </div>
+            </div>
+        </div>
+    
         );
       }
       
       function ManuallyEnterContractContent() {
         return (
-          <div>
-            <span className="p-float-label">
-              <InputText id="heading" />
-              <label htmlFor="heading">Heading</label>
-            </span>
-            {/* clause */}
-            <span className="p-float-label">
-              <InputText id="clause" />
-              <label htmlFor="clause">Clause</label>
-            </span>
-          </div>
+            <div>   
+                <div className="spacer" style={{height:"20px"}}></div>      
+                <div className="flex align-items-center justify-content-center">
+                    <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+                        <div className="text-center mb-5">
+                            <img src="/demo/images/blocks/logos/hyper.svg" alt="hyper" height={50} className="mb-3" />
+                            <div className="text-900 text-3xl font-medium mb-3">Manually create the contract</div>
+                        </div>
+                        <div>
+
+                            <label htmlFor="heading" className="block text-900 font-medium mb-2">Heading</label>
+                            <InputText id="heading" placeholder="Heading of the contract" className="w-full mb-3"/>
+                            <div className="spacer" style={{height:"20px"}}></div>
+
+                            {/* clause */}
+                            <label htmlFor="clause" className="block text-900 font-medium mb-2">Clause</label>
+                            <InputText id="clause" placeholder="Enter the clause" className="w-full mb-3"/>
+                            <div className="spacer" style={{height:"20px"}}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
         );
       }
       
-
     return (
         <div>
           <Dialog header="Header" visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)} footer={footerContent}>
-            {selectedContent === "paste" ? (
-              <PasteContractContent />
-            ) : selectedContent === "manually" ? (
-              <ManuallyEnterContractContent />
-            ) : (
               <p className="m-0">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-            )}
           </Dialog>
+          
+          {selectedContent === "paste" ? (<PasteContractContent />) : selectedContent === "manually" ? (
+              <ManuallyEnterContractContent />
+            ) :(
+                <p>
+
+                </p>
+            )}
         </div>
       );
       
